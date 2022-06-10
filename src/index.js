3a3e40f6b0f542ddd36064cc6b499e39030924c3
Janice Alecha <janiceralecha@gmail.com>
2022-06-09 21:08:10 -0500

    commit tree 9c8a802e4996cb0042d15ab2ed0c5f6bbf679cd9

--- src/index.js
+++ src/index.js
@@ -1,1 +1,64 @@
 console.log('%c HI', 'color: firebrick')
+function init() {
+    partA()
+    partB()
+    partC()
+}
+
+function partA() {
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then (res => res.json())
+.then (data => {
+        for (i of data.message) {
+
+            let a = document.createElement('img')
+            a.src=i
+            document.getElementById('dog-image-container').appendChild(a)
+        }
+})
+}
+
+function partB() {
+    url = "https://dog.ceo/api/breeds/list/all"
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        for (i in data.message) { 
+            let a = document.createElement('li')
+            a.innerText=i
+            a.addEventListener('click', function(e) {
+                e.target.style.color="red"
+            })
+            document.getElementById('dog-breeds').appendChild(a)
+
+         }
+    })
+}
+
+function partC() {
+    drop = document.getElementById('breed-dropdown')
+    drop.addEventListener('change', function(e) {
+        //console.log(e.target.value)
+        list = document.getElementById('dog-breeds')
+        while(list.firstChild) { list.firstChild.remove(); }
+
+        url = "https://dog.ceo/api/breeds/list/all"
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            for (i in data.message) { 
+                let a = document.createElement('li')
+                a.innerText=i
+                a.addEventListener('click', function(e) {
+                    e.target.style.color="red"
+                })
+                if (i[0] ===e.target.value) {
+                document.getElementById('dog-breeds').appendChild(a) }
+
+             }
+        })
+
+    })
+}
+
+document.addEventListener("DOMContentLoaded", init)
